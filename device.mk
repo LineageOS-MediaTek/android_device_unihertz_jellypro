c8dd1874986034e8570e4a176afcff3bdd2e1235
SamarV-121 <samarvispute121@gmail.com>
2018-04-26 11:46:41 +0530

    nicklaus: Build light HAL
    * also Rearrange device.mk
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- device.mk
+++ device.mk
@@ -22,16 +22,17 @@ FINGERPRINT_SUPPORT := true
 PRODUCT_PACKAGES += \
     power.mt6737m
 
+# Lights
+PRODUCT_PACKAGES += \
+    lights.mt6737m
+
 # Symbols
 PRODUCT_PACKAGES += \
     libshims
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_device.xml:system/vendor/etc/audio_device.xml \
-    $(LOCAL_PATH)/configs/audio/audio_em.xml:system/vendor/etc/audio_em.xml \
-    $(LOCAL_PATH)/configs/audio/AudioParamOptions.xml:/system/vendor/etc/audio_param/AudioParamOptions.xml \
-    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/vendor/etc/audio_policy.conf
+    $(call find-copy-subdir-files,*,${LOCAL_PATH}/configs/audio,system/vendor/etc)
 
 # Bluetooth
 PRODUCT_COPY_FILES += \
@@ -47,32 +48,22 @@ PRODUCT_COPY_FILES += \
 
 # Keyboard layout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/keylayout/mtk-kpd.kl:system/usr/keylayout/mtk-kpd.kl \
-    $(LOCAL_PATH)/configs/keylayout/ACCDET.kl:system/usr/keylayout/ACCDET.kl \
-    $(LOCAL_PATH)/configs/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
-    $(LOCAL_PATH)/configs/keylayout/uinput-egis.kl:system/usr/keylayout/uinput-egis.kl
+    $(call find-copy-subdir-files,*,${LOCAL_PATH}/configs/keylayout,system/usr/keylayout)
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/idc/uinput-egis.idc:system/usr/idc/uinput-egis.idc
+    $(call find-copy-subdir-files,*,${LOCAL_PATH}/configs/idc,system/usr/idc)
 
 # Telephony
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/telephony/cdma_ecc_list.xml:system/vendor/etc/cdma_ecc_list.xml \
-    $(LOCAL_PATH)/configs/telephony/cdma_ecc_list_ss.xml:system/vendor/etc/cdma_ecc_list_ss.xml
+    $(call find-copy-subdir-files,*,${LOCAL_PATH}/configs/telephony,system/vendor/etc)
 
 # Misc
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/misc/custom.conf:system/vendor/etc/custom.conf \
-    $(LOCAL_PATH)/configs/misc/clatd.conf:system/etc/clatd.conf \
-    $(LOCAL_PATH)/configs/misc/perfservapplist.txt:system/vendor/etc/perfservapplist.txt \
-    $(LOCAL_PATH)/configs/misc/perfservscntbl.txt:system/vendor/etc/perfservscntbl.txt \
-    $(LOCAL_PATH)/configs/misc/slp_conf:system/vendor/etc/slp_conf \
-    $(LOCAL_PATH)/configs/misc/resmonwhitelist.txt:system/vendor/etc/resmonwhitelist.txt \
-    $(LOCAL_PATH)/configs/misc/factory.ini:system/vendor/etc/factory.ini \
-    $(LOCAL_PATH)/configs/misc/mtklog-config.prop:system/vendor/etc/mtklog-config.prop
+    $(call find-copy-subdir-files,*,${LOCAL_PATH}/configs/misc,system/vendor/etc) \
+    $(LOCAL_PATH)/configs/misc/clatd.conf:system/etc/clatd.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/radvd/radvd.conf:system/vendor/etc/radvd.conf
+    $(LOCAL_PATH)/configs/radvd/radvd.conf:system/vendor/etc/radvd/radvd.conf
 
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,${LOCAL_PATH}/configs/wide-dhcpv6,system/vendor/etc/wide-dhcpv6)
@@ -85,24 +76,11 @@ PRODUCT_COPY_FILES += \
 
 # Ramdisk
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/init.project.rc:root/init.project.rc \
-    $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
-    $(LOCAL_PATH)/rootdir/ueventd.mt6735.rc:root/ueventd.mt6735.rc \
-    $(LOCAL_PATH)/rootdir/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
-    $(LOCAL_PATH)/rootdir/init.mt6735.rc:root/init.mt6735.rc \
-    $(LOCAL_PATH)/rootdir/fstab.mt6735:root/fstab.mt6735 \
-    $(LOCAL_PATH)/rootdir/enableswap.sh:root/enableswap.sh \
-    $(LOCAL_PATH)/rootdir/sbin/busybox:root/sbin/busybox \
-    $(LOCAL_PATH)/rootdir/init.microtrust.rc:root/init.microtrust.rc \
-    $(LOCAL_PATH)/rootdir/init.connectivity.rc:root/init.connectivity.rc \
-    $(LOCAL_PATH)/rootdir/init.mt6735.power.rc:root/init.mt6735.power.rc
+    $(call find-copy-subdir-files,*,${LOCAL_PATH}/rootdir,root)
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal/.ht120.mtc:system/vendor/etc/.tp/.ht120.mtc \
-    $(LOCAL_PATH)/configs/thermal/thermal.conf:system/vendor/etc/.tp/thermal.conf \
-    $(LOCAL_PATH)/configs/thermal/thermal.off.conf:system/vendor/etc/.tp/thermal.off.conf \
-    $(LOCAL_PATH)/configs/thermal/thermal_screenoff_full.conf:system/etc/vendor/.tp/thermal_screenoff_full.conf
+    $(call find-copy-subdir-files,*,${LOCAL_PATH}/configs/thermal,system/vendor/etc/.tp)
 
 # Extras
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
