d23f12268f435cedece15f1e54f82471db8f56c5
SamarV-121 <samarvispute121@gmail.com>
2018-08-07 02:12:09 -0400

    jellypro: Add kernel source to build libs
    * https://github.com/LineageOS/android_kernel_cyanogen_mt6735
    * since we dont have kernel source
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,12 +49,20 @@ TARGET_SCREEN_HEIGHT := 432
 TARGET_SCREEN_WIDTH := 240
 
 # Kernel
+TARGET_USES_64_BIT_BINDER := true
+TARGET_IS_64_BIT := true
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x40078000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --base 0x40078000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x03f88000 --second_offset 0x00e88000 --tags_offset 0x0df88000 --board 1480056755
+TARGET_KERNEL_SOURCE := kernel/cyanogen/mt6735
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CONFIG := cyanogenmod_porridge_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+MTK_APPENDED_DTB_SUPPORT := yes
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel.gz
-BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
@@ -89,4 +97,4 @@ BOARD_USES_MTK_HARDWARE := true
 # BOARD_SEPOLICY_DIRS := $(COMMON_PATH)/sepolicy
 
 # Hack for building without kernel sources
-$(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
+# $(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
