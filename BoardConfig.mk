2d4a527406c1363d0e3d7680d687f2d29735d8af
SamarV-121 <samarvispute121@gmail.com>
2018-08-10 02:06:46 -0400

    jellypro: Cleanup
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,18 @@
-# mt6737 platform boardconfig
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 DEVICE_PATH := device/unihertz/jellypro
 
 # TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
@@ -49,7 +63,6 @@ TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel.gz
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
-# BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3925868544
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 1610612736
 BOARD_CACHEIMAGE_PARTITION_SIZE := 419430400
--- lineage.mk
+++ lineage.mk
@@ -1,21 +1,26 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Inherit some common lineageos stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Release name
-PRODUCT_RELEASE_NAME := jellypro
-
 PRODUCT_DEVICE := jellypro
 PRODUCT_NAME := lineage_jellypro
 PRODUCT_BRAND := unihertz
 PRODUCT_MODEL := jellypro
 PRODUCT_MANUFACTURER := jellypro
-
-# Boot animation
-TARGET_SCREEN_HEIGHT      := 432
-TARGET_SCREEN_WIDTH       := 240
-
-PRODUCT_DEFAULT_LANGUAGE := en
-PRODUCT_DEFAULT_REGION   := US
